e5938af044fae1f3d505f7ab9d63be8819a62a84
James Forster <james.forster@justeattakeaway.com>
2020-11-30 16:16:13 +0000

    sets up game class and test project

---
+++ gameOfLife/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
